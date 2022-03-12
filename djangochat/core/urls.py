108ad5d798801d725f798a5d52c6716a02d86c34
Adebayo Osinulu <bilardosinulu@gmail.com>
2022-03-12 20:00:05 +0100

    Implemented logout functionality

--- djangochat/core/urls.py
+++ djangochat/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from .views import *
 
 urlpatterns = [
     path("", homepage, name="homepage"),
     path("sign-up/", signUp, name="sign-up"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
