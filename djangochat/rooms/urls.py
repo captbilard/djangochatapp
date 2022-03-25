07500edf38c22156b335b4ed35bd265756b4d99b
Adebayo Osinulu <bilardosinulu@gmail.com>
2022-03-25 17:09:53 +0100

    Create Detailed View for each room

--- djangochat/rooms/urls.py
+++ djangochat/rooms/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.rooms, name="rooms"),
+    path("<slug:room_slug>", views.room, name="room"),
 ]
--- djangochat/rooms/views.py
+++ djangochat/rooms/views.py
@@ -10,4 +10,10 @@ def rooms(request):
     all_rooms = Rooms.objects.all()
     context = {'rooms': all_rooms}
     return render(request, "rooms/index.html", context)
+
+@login_required()
+def room (request, room_slug):
+    room  = Rooms.objects.get(slug=room_slug)
+    context  = {'room': room}
+    return render(request, "rooms/room.html", context)
  
